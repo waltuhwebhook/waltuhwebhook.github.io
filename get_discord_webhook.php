41d4fdedad03298e50996f4a7f93bbb906f1d85e
waltuhwebhook <126408697+waltuhwebhook@users.noreply.github.com>
2023-02-26 16:39:30 -0500

    Update get_discord_webhook.php

--- get_discord_webhook.php
+++ get_discord_webhook.php
@@ -1,5 +1,15 @@
 <?php
 $discord_webhook_secret = $_ENV['WEBHOOK'];
 $discord_webhook_url = 'https://discord.com/api/webhooks/' . $discord_webhook_secret;
-echo $discord_webhook_url;
+$message = array('content' => $_POST['message']);
+$options = array(
+  'http' => array(
+    'method' => 'POST',
+    'header' => 'Content-Type: application/json',
+    'content' => json_encode($message)
+  )
+);
+$context = stream_context_create($options);
+$result = file_get_contents($discord_webhook_url, false, $context);
+echo $result;
 ?>
